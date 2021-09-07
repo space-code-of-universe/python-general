8606759d9e9f2f72c7a0667f119b69af05e01ed6
Setyo Pambudi <setyopambudi34@gmail.com>
2021-09-07 08:34:33 +0700

    simple calculator minor

--- simple_calculator.py
+++ simple_calculator.py
@@ -4,14 +4,27 @@ numb_1 = float(input('Input your first number ! '))
 operator = input('Insert +, -, *, / ')
 numb_2 = float(input('Input your second number ! '))
 if operator=='+':
-    print(f'Hasilnya adalah {numb_1 + numb_2}')
+    result = numb_1 + numb_2
+    print(f'The result is {result}')
+
 elif operator=='-':
-    print(f'Hasilnya adalah {numb_1 - numb_2}')
+    result = numb_1 - numb_2
+    print(f'The result is {result}')
+
 elif operator =='*' or 'x':
-    print(f'Hasilnya adalah {numb_1 * numb_2}')
+    result = numb_1 * numb_2
+    print(f'The result is {result}')
+
 elif operator=='/':
-    print(f'Hasilnya adalah {numb_1 / numb_2}')
+    result = numb_1 / numb_2
+    print(f'The result is {result}')
+
+else:
+    print('Please input the correct number')
+
 print('Thank you')
+
+
 
 
 
